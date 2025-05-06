4f8c1e8f7141785d7a891fb4602d80edc350f794
vieirabmp <75789461+vieirabmp@users.noreply.github.com>
2025-05-06 18:58:47 +0100

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -11,8 +11,15 @@ namespace Calculos
         static void Main(string[] args)
         {
             Console.WriteLine("Aplicação Cálculos");
+
             Console.WriteLine($"4 + 2 = {Aritmetica.Somar(4, 2)}");
             Console.WriteLine($"4 + 2 = {Aritmetica.Subtrair(4, 2)}");
+
+            double t1 = Conversoes.ConversaoTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversoes.ConversaoTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
 
             Console.ReadKey();
         }
